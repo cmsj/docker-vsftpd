c12d5ba90b0b3be54383cb7566422acf9a717eca
Chris Jones <cmsj@tenshu.net>
2025-04-12 22:37:10 +0100

    Attempt to fix missing logs

--- src/vsftpd.sh
+++ src/vsftpd.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+LOGFILE=/var/log/vsftpd.log
+XFERFILE=/var/log/xferlog
+
+touch "${LOGFILE}"
+touch "${XFERFILE}"
+
+tail -Ff "${LOGFILE}" | tee -a /dev/fd/1 &
+tail -Ff "${XFERFILE}" | tee -a /dev/fd/1 &
+
 /usr/sbin/vsftpd /etc/vsftpd.conf
